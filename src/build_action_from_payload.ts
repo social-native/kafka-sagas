3c20b2e98ca9c637836f2bf22fede4cf25a69b22
Ricky Sidhu <seeingsaturn@gmail.com>
2019-11-19 21:00:50 -0800

    chore: delete a comment

--- src/build_action_from_payload.ts
+++ src/build_action_from_payload.ts
@@ -6,10 +6,9 @@ export function buildActionFromPayload(topic: string, message: KafkaMessage) {
     const extracted = JSON.parse(message.value.toString());
 
     if (!isTransactionAction(extracted)) {
-            const transactionId = uuid.v4();
+        const transactionId = uuid.v4();
 
-            return {topic, ...extracted, transactionId};
-            // throw new Error('Message is missing either transactionId and/or payload, idiot');
+        return {topic, ...extracted, transactionId};
     }
 
     return {topic, ...extracted};
