191c932c112398ef1a0ddedd88a64dd80357460e
Ricky Sidhu <seeingsaturn@gmail.com>
2019-11-19 21:15:02 -0800

    refactor: effect runner to recursion

--- src/create_effect_runner.ts
+++ src/create_effect_runner.ts
@@ -6,6 +6,46 @@ export function createEffectRunner(
     consumerMessageBus: ConsumerMessageBus,
     producerMessageBus: ProducerMessageBus
 ) {
+
+    // tslint:disable-next-line: cyclomatic-complexity
+    async function runGeneratorFsm(machine: Generator, lastValue: any = null): Promise<any> {
+        const {done, value: cause}: IteratorResult<unknown> = machine.next(lastValue);
+
+        if (done) {
+            return lastValue;
+        }
+
+        if (isTakeCause(cause)) {
+            for (const topic of cause.patterns) {
+                // Add the transactionId to the transactions we are watching out for.
+                consumerMessageBus.startTransaction(cause.transactionId);
+                await consumerMessageBus.addSubscription(topic);
+                const response = await consumerMessageBus.awaitEventBroadcast(
+                    topic,
+                    cause.transactionId
+                );
+
+                // The broadcasted result will be sent back to the saga.
+                return runGeneratorFsm(machine, response);
+            }
+        }
+
+        if (isPutCause(cause)) {
+            await producerMessageBus.putPayloadToTopic(
+                cause.pattern,
+                cause.payload
+            );
+
+            return runGeneratorFsm(machine);
+        }
+
+        if (isCallCause(cause)) {
+            const response = await cause.effect(...cause.args);
+
+            return runGeneratorFsm(machine, response);
+        }
+    }
+
     return {
         // tslint:disable-next-line: cyclomatic-complexity
         async runEffects<Context extends IBaseSagaContext>(
@@ -13,47 +53,7 @@ export function createEffectRunner(
             context: Context,
             saga: GeneratorFunction
         ) {
-            const runningSaga = saga(initialAction, context);
-            let previousValue = null;
-
-            while (true) {
-                const {done, value: cause}: IteratorResult<unknown> = runningSaga.next(
-                    previousValue
-                );
-
-                if (isTakeCause(cause)) {
-                    for (const topic of cause.patterns) {
-                        // Add the transactionId to the transactions we are watching out for.
-                        consumerMessageBus.startTransaction(cause.transactionId);
-                        await consumerMessageBus.addSubscription(topic);
-                        const response = await consumerMessageBus.awaitEventBroadcast(
-                            topic,
-                            cause.transactionId
-                        );
-
-                        // The broadcasted result will be sent back to the saga.
-                        previousValue = response;
-                    }
-
-                    continue;
-                }
-
-                if (isPutCause(cause)) {
-                    await producerMessageBus.putPayloadToTopic(cause.pattern, cause.payload);
-                    continue;
-                }
-
-                if (isCallCause(cause)) {
-                    const response = await cause.effect(...cause.args);
-
-                    previousValue = response;
-                    continue;
-                }
-
-                if (done) {
-                    break;
-                }
-            }
+            return await runGeneratorFsm(saga(initialAction, context));
         }
     };
 }
