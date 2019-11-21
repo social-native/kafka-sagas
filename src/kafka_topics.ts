76f6bdbc5c8475c11da947957d5fa00d0fbac2ec
dearsaturn <seeingsaturn@gmail.com>
2019-11-20 18:01:47 -0800

    pseudocode: kafka_topics.ts
    Co-authored-by: Ethan Hathaway <erhathaway@gmail.com>

---
+++ src/kafka_topics.ts
@@ -0,0 +1,35 @@
+export default {
+    topics: {
+        cloneCampaign: {
+            start: 'CLONE_CAMPAIGN_START',
+            success: 'CLONE_CAMPAIGN_SUCCESS',
+            failure: 'CLONE_CAMPAIGN_FAILURE'
+        },
+        copyCampaign: {
+            start: 'COPY_CAMPAIGN_START',
+            success: 'COPY_CAMPAIGN_SUCCESS',
+            failure: 'COPY_CAMPAIGN_FAILURE'
+        },
+        copyProductions: {
+            start: 'COPY_PRODUCTIONS_START',
+            success: 'COPY_PRODUCTIONS_SUCCESS',
+            failure: 'COPY_PRODUCTIONS_FAILURE'
+        },
+        copyDirections: {
+            start: 'COPY_DIRECTIONS_START',
+            success: 'COPY_DIRECTIONS_SUCCESS',
+            failure: 'COPY_DIRECTIONS_FAILURE'
+        }
+    },
+    generateTopicForTransaction(transactionId: string, topic: string) {
+        return `saga-tx-${transactionId}-${topic}`;
+    },
+    matchTopicForTransaction(topic: string) {
+        const re = new RegExp('saga-tx-.+-(.+)');
+        const captures = topic.match(re);
+        if (captures) {
+            return captures[1];
+        }
+        return undefined;
+    }
+};
