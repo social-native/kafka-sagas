2eab5b5423b2fbbb697c2a81b6240466ccde2be4
Ricky Sidhu <seeingsaturn@gmail.com>
2019-12-05 17:20:56 -0800

    fix: missing await, cleanup

--- src/__tests__/consumer_message_bus.test.ts
+++ src/__tests__/consumer_message_bus.test.ts
@@ -1,51 +1,46 @@
 import Bluebird from 'bluebird';
 import {ConsumerMessageBus} from '../../src/consumer_message_bus';
 import {kafka} from './test_clients';
-import {createTopic, seedTopic, deleteTopic} from './kafka_utils';
+import {seedTopic, withTopicCleanup} from './kafka_utils';
 import {IAction} from 'types';
 
 describe(ConsumerMessageBus.name, function() {
     it('notifies observers of new messages only', async function() {
-        const topic = 'bart-report-card-arrived';
-        const transactionId = 'super-cool-transaction';
+        await withTopicCleanup(['bart-report-card-arrived'])(async ([topic]) => {
+            const transactionId = 'super-cool-transaction';
 
-        expect(topic).toEqual(topic);
-        expect(transactionId).toEqual(transactionId);
-
-        const preseededMessage: Partial<IAction> = {
-            transaction_id: transactionId,
-            payload: {country: 'france'}
-        };
-
-        await createTopic(topic);
-        await seedTopic(topic, [preseededMessage]);
-
-        const bus = new ConsumerMessageBus(kafka, 'consumer_message_bus_test');
-        const receivedMessages: IAction[] = [];
-        bus.startTransaction(transactionId);
-
-        bus.registerTopicObserver({
-            transactionId,
-            topic,
-            observer: action => receivedMessages.push(action)
-        });
-
-        await bus.streamActionsFromTopic(topic);
-
-        await seedTopic(topic, [
-            {
+            const preseededMessage: Partial<IAction> = {
                 transaction_id: transactionId,
-                payload: {new_message: true}
-            }
-        ]);
+                payload: {country: 'france'}
+            };
 
-        // give it some time to deliver
-        await Bluebird.delay(1000);
+            await seedTopic(topic, [preseededMessage]);
 
-        await bus.disconnectConsumers();
+            const bus = new ConsumerMessageBus(kafka, 'consumer_message_bus_test');
+            const receivedMessages: IAction[] = [];
+            bus.startTransaction(transactionId);
 
-        await deleteTopic(topic);
+            bus.registerTopicObserver({
+                transactionId,
+                topic,
+                observer: action => receivedMessages.push(action)
+            });
 
-        expect(receivedMessages.map(({payload}) => payload)).toContainEqual({new_message: true});
+            await bus.streamActionsFromTopic(topic);
+
+            await seedTopic(topic, [
+                {
+                    transaction_id: transactionId,
+                    payload: {new_message: true}
+                }
+            ]);
+
+            // give it some time to deliver
+            await Bluebird.delay(300);
+
+            await bus.disconnectConsumers();
+
+            expect(receivedMessages.map(({payload}) => payload)).toContainEqual({new_message: true});
+        });
     }, 8000);
 });
