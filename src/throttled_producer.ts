6925c9523ce3ad0a762e952646fee44940353dd9
Ricky Sidhu <seeingsaturn@gmail.com>
2020-08-26 15:01:16 -0700

    fix: retry delay, flush behavior, interval timing

--- src/throttled_producer.ts
+++ src/throttled_producer.ts
@@ -1,28 +1,41 @@
 import {Producer, Kafka, CompressionTypes, ProducerConfig} from 'kafkajs';
-import {IAction, IOutgoingRecord} from './types';
+import {IAction, IQueuedRecord} from './types';
 import {createActionMessage} from './create_action_message';
 import {TopicAdministrator} from './topic_administrator';
 import {isKafkaJSProtocolError} from './type_guard';
+import Bluebird from 'bluebird';
+import pino from 'pino';
+import uuid from 'uuid';
 
 export class ThrottledProducer {
+    public recordsSent = 0;
+
     private producer: Producer;
     private topicAdministrator: TopicAdministrator;
     private isConnected: boolean = false;
     private intervalTimeout: NodeJS.Timeout;
     private createdTopics = new Set<string>();
-    private recordOutbox: IOutgoingRecord[] = [];
-    private isFlushingOutbox = false;
+    private recordQueue: IQueuedRecord[] = [];
+    private isFlushing = false;
+    private logger: pino.Logger;
 
     constructor(
         protected kafka: Kafka,
         protected producerConfig: Omit<
             ProducerConfig,
             'allowAutoTopicCreation' | 'maxInFlightRequests' | 'idempotent'
-        > & {maxOutgoingBatchSize: number} = {maxOutgoingBatchSize: 1000},
-        topicAdministrator?: TopicAdministrator
+        > & {maxOutgoingBatchSize?: number; flushIntervalMs?: number} = {
+            maxOutgoingBatchSize: 10000,
+            flushIntervalMs: 1000
+        },
+        topicAdministrator?: TopicAdministrator,
+        logger?: pino.Logger
     ) {
-        this.createProducer();
         this.topicAdministrator = topicAdministrator || new TopicAdministrator(kafka);
+        this.logger = logger
+            ? logger.child({class: 'KafkaSagasThrottledProducer'})
+            : pino().child({class: 'KafkaSagasThrottledProducer'});
+        this.createProducer();
     }
 
     // tslint:disable-next-line: cyclomatic-complexity
@@ -32,33 +45,26 @@ export class ThrottledProducer {
         }
 
         if (!this.createdTopics.has(action.topic)) {
+            this.logger.debug({topic: action.topic}, 'Creating topic');
             await this.topicAdministrator.createTopic(action.topic);
             this.createdTopics.add(action.topic);
         }
 
-        let _resolve: IOutgoingRecord['resolve'] | undefined;
-        let _reject: IOutgoingRecord['reject'] | undefined;
-
-        const promise = new Promise<any>((resolve, reject) => {
-            _resolve = resolve;
-            _reject = reject;
-        });
-
-        this.recordOutbox = [
-            ...this.recordOutbox,
-            {
-                resolve: _resolve as any,
-                reject: _reject as any,
-                record: {
-                    acks: -1,
-                    compression: CompressionTypes.GZIP,
-                    topic: action.topic,
-                    messages: [createActionMessage({action})]
+        return new Promise<void>((resolve, reject) => {
+            this.recordQueue = [
+                ...this.recordQueue,
+                {
+                    resolve,
+                    reject,
+                    record: {
+                        topic: action.topic,
+                        messages: [createActionMessage({action})]
+                    }
                 }
-            }
-        ];
+            ];
 
-        return promise;
+            return;
+        });
     };
 
     public connect = async () => {
@@ -66,11 +72,15 @@ export class ThrottledProducer {
             return;
         }
 
-        await this.producer.connect();
+        const flushIntervalMs = this.producerConfig.flushIntervalMs || 1000;
 
-        this.intervalTimeout = setInterval(async () => {
-            await this.flushOutbox();
-        }, 100);
+        this.logger.debug('Connecting producer');
+        await this.producer.connect();
+        this.logger.debug('Connected producer');
+
+        this.logger.debug({flushIntervalMs}, 'Creating flush interval');
+        this.intervalTimeout = setInterval(this.flush, flushIntervalMs);
+        this.logger.debug('Created flush interval');
 
         this.isConnected = true;
     };
@@ -80,72 +90,99 @@ export class ThrottledProducer {
             return;
         }
 
+        this.logger.debug('Disconnecting');
         clearInterval(this.intervalTimeout);
 
         await this.producer.disconnect();
+        this.logger.debug('Disconnected');
         this.isConnected = false;
     };
 
     private createProducer = () => {
+        this.logger.debug('Creating a new producer');
         this.producer = this.kafka.producer({
             maxInFlightRequests: 1,
             idempotent: true,
             ...this.producerConfig
         });
+        this.logger.debug('Created a new producer');
     };
 
     // tslint:disable-next-line: cyclomatic-complexity
-    private flushOutbox = async (retryRecord?: IOutgoingRecord[], retryCounter = 0) => {
-        if (this.isFlushingOutbox) {
+    private flush = async (
+        retryRecords?: IQueuedRecord[],
+        retryCounter = 0,
+        retryBatchId?: string
+    ) => {
+        if (!retryRecords && this.isFlushing) {
             return;
+        }
+
+        if (retryCounter) {
+            /** Wait for a max of 30 seconds before retrying */
+            const retryDelay = Math.min(retryCounter * 1000, 30000);
+            this.logger.debug({retryDelay}, 'Waiting before attempting retry');
+            await Bluebird.delay(retryDelay);
         }
 
         /**
          * Ensures that if the interval call ends up being concurrent due latency in sendBatch,
-         * another cycle is deferred to the next interval.
+         * unintentinally overlapping cycles are deferred to the next interval.
          */
-        this.isFlushingOutbox = true;
+        this.isFlushing = true;
 
-        const outgoingRecords =
-            retryRecord ||
-            this.recordOutbox.slice(0, this.producerConfig.maxOutgoingBatchSize || 1000);
+        const batchSize = this.producerConfig.maxOutgoingBatchSize || 1000;
+        const outgoingRecords = retryRecords || this.recordQueue.slice(0, batchSize);
+        this.recordQueue = this.recordQueue.slice(batchSize);
+        const batchId = retryBatchId || uuid.v4();
 
-        if (!outgoingRecords) {
+        if (!outgoingRecords.length) {
+            this.logger.debug({batchId}, 'No records to flush');
+            this.isFlushing = false;
             return;
         }
 
+        this.logger.debug(
+            {
+                remaining: this.recordQueue.length,
+                records: outgoingRecords.length,
+                batchId
+            },
+            'Flushing queue'
+        );
+
         try {
             await this.producer.sendBatch({
-                topicMessages: outgoingRecords.map(({record}) => ({
-                    topic: record.topic,
-                    messages: record.messages
-                })),
+                topicMessages: outgoingRecords.map(({record}) => record),
                 acks: -1,
                 compression: CompressionTypes.GZIP
             });
 
+            this.recordsSent += outgoingRecords.length;
+            this.logger.debug({batchId}, 'Flushed queue');
             outgoingRecords.map(({resolve}) => resolve());
+            this.isFlushing = false;
+            return;
         } catch (error) {
             /**
              * If for some reason this producer is no longer recognized by the broker,
              * create a new producer.
              */
-            if (
-                isKafkaJSProtocolError(error) &&
-                error.type === 'UNKNOWN_PRODUCER_ID' &&
-                retryCounter < 6
-            ) {
+            if (isKafkaJSProtocolError(error) && error.type === 'UNKNOWN_PRODUCER_ID') {
                 await this.producer.disconnect();
                 this.createProducer();
                 await this.producer.connect();
-                await this.flushOutbox(outgoingRecords, retryCounter + 1);
+                this.logger.debug(
+                    {batchId},
+                    'Retrying failed flush attempt due to UNKNOWN_PRODUCER_ID'
+                );
+                await this.flush(outgoingRecords, retryCounter + 1, batchId);
                 return;
             }
 
             outgoingRecords.map(({reject}) => reject(error));
+            this.isFlushing = false;
             return;
-        } finally {
-            this.isFlushingOutbox = false;
         }
     };
 }
